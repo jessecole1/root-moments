c9a988bf6b13c9de5d9f03b50cf9c40b9de4debb
jesse cole <cole.j.jesse@gmail.com>
2024-10-13 19:46:43 -0700

    updated cors

--- server/server.js
+++ server/server.js
@@ -4,8 +4,23 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+const allowedOrigins = [
+    'https://rootmoments.netlify.app',
+    'http://localhost:3000'
+]
+
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "https://rootmoments.netlify.app", methods: ['GET', 'POST'] }));
+app.use(cors({ 
+    credentials: true, 
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            return callback(new Error("CORS policy doesn't allow this origin"), false);
+        }
+        return callback(null, true);
+    }, 
+    methods: ['GET', 'POST'] }));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
