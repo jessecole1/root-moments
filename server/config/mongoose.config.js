4676859c80819a5d9eb4ad986dbcf6d74b4724c9
jesse cole <cole.j.jesse@gmail.com>
2024-10-13 18:53:32 -0700

    changed to mongodb atlas

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/root-moments", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Established a connection to the database."))
-.catch(err => console.log("Something went wrong when connecting to the database", err))
-
-// mongoose.connect(process.env.MONGO_URI, {
+// mongoose.connect("mongodb://127.0.0.1:27017/root-moments", {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
 // })
-// .then(() => console.log('MongoDB connected successfully'))
-// .catch((error) => console.error('Error connecting to MongoDB:', error));
+// .then(() => console.log("Established a connection to the database."))
+// .catch(err => console.log("Something went wrong when connecting to the database", err))
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
